75b43f34a04e71edd3468948fd4ac30c826a06b9
Huh-jae-won <58212928+Huh-jae-won@users.noreply.github.com>
2019-11-26 22:59:36 +0900

    deci to bin

--- 20192/DataStructure/deci_to_bin.c
+++ 20192/DataStructure/deci_to_bin.c
@@ -1,9 +1,12 @@
+// stack을 구현하여 헤더파일로 만들고 
+// stack을 사용할만한 예제를 만듦
 #include <stdio.h>
 #include <stdbool.h>
 #include "stacksADT.h"
-#pragma warning(disable:4996)
+#pragma warning(disable:4996)	// scanf_s말고 scanf를 사용하기 위해
 
 int main(void) {
+	// 10진수를 2진수로 바꾸는 프로그램 구현
 	unsigned int num;
 	int* digit;
 	STACK* stack2;
@@ -15,11 +18,11 @@ int main(void) {
 	while (num>0) {
 		digit = (int*)malloc(sizeof(int));
 		*digit = num % 2;
-		pushStack(stack2, digit);
+		pushStack(stack2, digit);		// num을 2진수로 나누고 난 나머지를 스택에 저장
 		num = num / 2;
 	}
 	printf("bin_num is : ");
-	while (!emptyStack(stack2)) {
+	while (!emptyStack(stack2)) {			// 스택에서 하나씩 꺼내며 출력
 		digit = (int*)popStack(stack2);
 		printf("%1d", *digit);
 	}
