a76d56aa3a6f2ad23c1ccebbe0b0a9a67919deb5
Huh-jae-won <58212928+Huh-jae-won@users.noreply.github.com>
2021-04-11 23:00:17 +0900

    Add files via upload

---
+++ 자율과제형/Inflearn/A_String&Array/Q06_MeetingRoom2.java
@@ -0,0 +1,83 @@
+package A_String_Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Interval06 {
+	int start;
+	int end;
+	Interval06() {
+		start = 0;
+		end = 0;
+	}
+	Interval06(int s, int e) {
+		start = s;
+		end = e;
+	}
+}
+
+public class Q06_MeetingRoom2 {
+	public static void main(String[] args) {
+		Q06_MeetingRoom2 q06 = new Q06_MeetingRoom2();
+		Interval06[] arr = new Interval06[4];
+		arr[0] = new Interval06(0,30);
+		arr[1] = new Interval06(5,15);
+		arr[2] = new Interval06(4,9);
+		arr[3] = new Interval06(10,16);
+//		int ret = q06.solve(arr);
+//		System.out.println(ret);
+		System.out.println(q06.solve_sol(arr));
+
+	}
+	public int solve_sol(Interval06[] intervals) {
+		Arrays.sort(intervals, comp);
+		// 우선순위 큐 오름차순 정렬?
+		Queue<Interval06> pq = new PriorityQueue<>((Interval06 a,Interval06 b) -> a.end-b.end);
+		pq.offer(intervals[0]);
+		for(int i=1 ; i<intervals.length ; i++) {
+			if(pq.peek().end <= intervals[i].start) {
+				pq.poll();
+			}
+			pq.offer(intervals[i]);
+		}		
+//		while(!pq.isEmpty()) {
+//			Interval06 temp = pq.poll();
+//			System.out.println(temp.start+" ~ "+temp.end);
+//		}
+		return pq.size();
+	}
+	
+	public int solve(Interval06[] intervals) {
+		Interval06[] ret = new Interval06[intervals.length];
+		int cnt = intervals.length;	// 회의실 최대 갯수
+		Arrays.sort(intervals, comp);
+		print(intervals);
+		
+		for(int i=0 ; i<intervals.length-1 ; i++) {
+			if(intervals[i].start+intervals[i].end==0) {
+				continue;
+			}
+			for(int j=i+1 ; j<intervals.length ; j++) {
+				if(intervals[i].end<=intervals[j].start) {
+					intervals[i].end = intervals[j].end;
+					intervals[j].start = 0;
+					intervals[j].end = 0;
+					cnt--;	// merge될때마다 회의실 한개 감소
+				}
+			}
+		}
+		return cnt;
+	}
+	
+	Comparator<Interval06> comp = new Comparator<>() {
+		public int compare(Interval06 a, Interval06 b) {
+			return a.start - b.start;	// 오름차순 정렬
+		}
+	};
+	public void print(Interval06[] intervals) {
+		for(int i=0 ; i<intervals.length ; i++) {
+			System.out.println(intervals[i].start+" ~ "+intervals[i].end);
+		}
+	}
+}
---
+++ 자율과제형/Inflearn/A_String&Array/Q15_FindAllAnagrams.java
@@ -0,0 +1,93 @@
+package A_String_Array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Q15_FindAllAnagrams {
+
+	public static void main(String[] args) {
+		Q15_FindAllAnagrams q15 = new Q15_FindAllAnagrams();
+		List<Integer> list = new ArrayList<>();
+
+		String s = "BACDGABCDA";
+		String p = "ABCD";
+		
+		list = q15.findAnagrams(s, p);
+//		list = q15.solve_sol(s, p);
+		System.out.println("===result===");
+		System.out.println(list);
+	}
+	public List<Integer> solve_sol(String txt, String pat){
+		List<Integer> result = new ArrayList<>();
+		// 초기 조건 부실
+		if(txt==null || txt.length()==0 || pat.length()==0 || txt.length()<pat.length()) {
+			return result;
+		}
+		
+		// 패턴에 해당하는 아스키코드값 index만 1로
+		int[] patArr = new int[256]; 
+		for(int i=0 ; i<pat.length() ; i++) {
+			patArr[pat.charAt(i)]++;
+		}
+		
+		for(int i=0 ; i<=txt.length()-pat.length() ; i++) {
+			int[] txtArr = new int[256];
+			for(int j=0 ; j<pat.length() ; j++) {
+				txtArr[txt.charAt(i+j)]++;
+			}
+			if(check(patArr, txtArr)) {
+				result.add(i);
+			}
+//			if(patArr.equals(txtArr)) {
+//				result.add(i);
+//			}
+		}
+		
+		return result;
+	}
+	private boolean check(int[] patArr, int[] txtArr) {
+		for(int i=0 ; i<patArr.length ; i++) {
+			if(patArr[i]!=txtArr[i])
+				return false;
+		}
+		return true;
+	}
+	
+	
+	public List<Integer> findAnagrams(String txt, String pat){
+		// 결과 담을 리스트
+		List<Integer> list = new ArrayList<>();
+		
+		// pat를 담은 set
+		Set<Character> patSet = new HashSet<>();
+		for(int i=0 ; i<pat.length() ; i++) {
+			patSet.add(pat.charAt(i));
+		}
+		
+		for(int i=0 ; i<=txt.length()-pat.length() ; i++) {
+			Set<Character> compareSet = new HashSet<>();
+			System.out.print(i+" : ");
+			
+			// pat길이 만큼 추출하여 set으로 만듦
+			for(int j=i ; j<i+pat.length() ; j++) {
+				compareSet.add(txt.charAt(j));
+			}
+			System.out.print(compareSet);
+			
+			// patSet과 compareSet이 동일하면 list에 index추가
+			if(compareSet.equals(patSet)) {
+				System.out.println(" + "+i+" 추가");
+				list.add(i);
+			}else {
+				System.out.println();
+			}
+		}
+		return list;
+	}
+
+}
